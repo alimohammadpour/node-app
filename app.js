494342406435129a90a15b2b75fe45336ac58b12
a.mohammadpour <a.mohammadpour@lan.local>
2025-01-06 15:58:46 +0100

    adds cors origin

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import router from './routes/routes.js';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 
+app.use(cors({
+    origin: process.env.CORS_ORIGIN || "*"
+}));
+  
 app.use('/api', router);
 
 export default app;
